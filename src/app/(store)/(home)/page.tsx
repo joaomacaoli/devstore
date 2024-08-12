44e05130cf11589392187c3f98cd5227bb24ccd1
João Macaoli <devjoaom@gmail.com>
2024-08-12 08:32:25 -0300

    update home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,8 +1,59 @@
-export default async function Home() {
+import Image from "next/image";
+import Link from "next/link";
 
-  await new Promise(resolve => setTimeout(resolve, 2000))
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
   return (
-    <h1>Hello!</h1>
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-never-stop-learning.png"
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129,00
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-java.png"
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          src="/moletom-ia-p-devs.png"
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+        />
+      </Link>
+    </div>
   );
 }
