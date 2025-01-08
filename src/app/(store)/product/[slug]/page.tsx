bd47feb346559210fafb24ec5d0eae82ed8e74c9
João Macaoli <devjoaom@gmail.com>
2025-01-08 13:49:53 -0300

    page metadata no nextjs

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
+import { Metadata } from "next";
 import Image from "next/image";
 
 interface ProductProps {
@@ -16,6 +17,16 @@ async function getProduct(slug: string): Promise<Product> {
   })
 
   return await response.json();
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+
+  return {
+    title: product.title
+  }
 }
 
 export default async function ProductPage({ params }: ProductProps) {
