22a148d2ec5ec44dd2a596a192c81e2d95b49a41
João Macaoli <devjoaom@gmail.com>
2025-01-08 09:51:08 -0300

    Fetch de dados no produto

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,8 +2,8 @@ import { env } from "@/env"
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-  const apiPath = '/api'.concat(path)
-  const url = new URL(apiPath, baseUrl)
+  const apiPath = '/api'
+  const url = new URL(apiPath.concat(path), baseUrl)
 
   return fetch(url, init)
 }
