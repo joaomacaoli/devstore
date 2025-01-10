b34a44c030482c6cb1df655595719920e2c78656
João Macaoli <devjoaom@gmail.com>
2025-01-10 09:04:56 -0300

    loading de busca

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import Skeleton from "@/components/skeleton"
+import { useSearchParams } from "next/navigation"
+
 export default function HomeLoading() {
-  return <p>Carregando busca...</p>
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <section className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <section className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+      </section>
+    </section>
+  )
 }
