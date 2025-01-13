478dae91921ba9858ad77cd63ce2b1e120de395a
João Macaoli <devjoaom@gmail.com>
2025-01-12 22:03:12 -0300

    adição do Suspense onde usa o hook useSearchParams

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,19 +1,11 @@
 'use client'
 
 import Skeleton from "@/components/skeleton"
-import { useSearchParams } from "next/navigation"
 
 export default function HomeLoading() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
   return (
     <section className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
-
+      <p className="text-sm">Carregando resultados...</p>
       <section className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[380px]" />
         <Skeleton className="h-[380px]" />
